cc811537192683cc2c5b2f7b742db3867b42d0cb
petermarkuska-afk <petermarkuska@gmail.com>
2026-03-01 10:28:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ OTAZKA = "Postavíme heliport?"
 HLAVNE_HESLO = "Victory2026" 
 MESACNY_PREDPIS = 10.0 
 # TU SI ZMEŇ DÁTUM KONCA (Formát RRRR-MM-DD):
-KONIEC_ANKETY = "2026-03-01"
+KONIEC_ANKETY = "2026-03-05"
 
 st.set_page_config(page_title="Správa areálu Victory Port", layout="centered", page_icon="🏡")
 
@@ -264,6 +264,7 @@ except Exception as e:
     st.error(f"Systémová informácia: {e}")
 
 st.markdown("<p style='text-align: center; font-size: 0.8em; color: gray; margin-top:50px;'>© 2026 Správa areálu Victory Port</p>", unsafe_allow_html=True)
+
 
 
 
